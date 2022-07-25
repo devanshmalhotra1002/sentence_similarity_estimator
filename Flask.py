2505eff91afeafa0bbc25c6188123aea25d95805
Devansh Malhotra <83959512+devanshmalhotra1002@users.noreply.github.com>
2022-07-25 06:25:21 +0530

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -22,8 +22,7 @@ def sentence_similarity():
         sentence2 = str(request.form['sentence2'])
 
         def clean(sentence):
-            clean = [i for i in sentence if i not in string.punctuation]
-            clean = "".join(clean)
+            clean = "".join([i for i in sentence if i not in string.punctuation])
             return clean
 
         def cosine_sim(vec1, vec2):
@@ -31,23 +30,22 @@ def sentence_similarity():
             return sim
 
         def embeddings_similarity(sentences):
-            pair = list(itertools.combinations(sentences, 2))
-            a = [pair[0] for pair in pair]
-            b = [pair[1] for pair in pair]
-            pair_df = pd.DataFrame({'a': a, 'b': b})
-            pair_df = pair_df.loc[pd.DataFrame(np.sort(
-                pair_df[['a', 'b']], 1), index=pair_df.index).drop_duplicates(keep='first').index]
-            pair_df = pair_df[pair_df['a'] != pair_df['b']]
+            combination = list(itertools.combinations(sentences, 2))
+            a = [pair[0] for pair in combination]
+            b = [pair[1] for pair in combination]
+            df = pd.DataFrame({'Sentence 1': a, 'Sentence 2': b})
+            df = df.loc[pd.DataFrame(np.sort(df[['Sentence 1', 'Sentence 2']], 1), index=df.index).drop_duplicates(keep='first').index]
+            df = df[pair_df['Sentence 1'] != pair_df['Sentence 2']]
 
             embeddings = spacy.load('en_core_web_sm')
 
-            pair_df['similarity'] = pair_df.apply(
+            df['similarity'] = df.apply(
                 lambda row: cosine_sim(
-                    embeddings(clean(row['a'])).vector,
-                    embeddings(clean(row['b'])).vector),
+                    embeddings(clean(row['Sentence 1'])).vector,
+                    embeddings(clean(row['Sentence 2'])).vector),
                 axis=1
             )
-            return pair_df['similarity'][0]
+            return df['similarity'][0]
         sentences = [sentence1, sentence2]
         sim = embeddings_similarity(sentences)
 
